4108404f12412e8957833c2bd9220c293ba3d735
mohamedhk2 <mohamedhk2@yahoo.fr>
2024-02-02 21:06:00 +0100

    first commit

---
+++ Pages/Customers.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyFirstApp.Models;
+
+namespace MyFirstApp.Pages
+{
+    public class CustomersModel : PageModel
+    {
+        private readonly MyDbContext dbContext;
+
+        public CustomersModel(MyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IList<Customer> Customers { get; set; }
+
+        public void OnGet()
+        {
+            Customers = dbContext.Customers
+                .Include(c => c.Orders)
+                //.ThenInclude(o => o.OrderStatus)
+                .ToList();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using MyFirstApp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register DbContext.
+builder.Services.AddDbContext<MyDbContext>();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
